f7b4e1f1944f1dbac61e0388e8f858b6e31d123d
Marcelo Matos <marcelofmatos@gmail.com>
2024-08-27 13:50:49 +0000

    feat: tgz

--- backup.sh
+++ backup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 BACKUP_FILE="/backup/$(date +\%Y-\%m-\%d_\%H-\%M-\%S).sql"
+COMPRESSED_FILE="/backup/$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tgz"
 
 S3_DIRECTORY_NAME=${S3_DIRECTORY_NAME:-"default-directory"}
 
 pg_dump -Fc -f $BACKUP_FILE
+tar -czf $COMPRESSED_FILE -C /backup $(basename $BACKUP_FILE)
 
-aws s3 cp $BACKUP_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$(basename $BACKUP_FILE) --region $S3_REGION
+aws s3 cp $COMPRESSED_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$(basename $COMPRESSED_FILE) --region $S3_REGION
 
-rm $BACKUP_FILE
+rm $BACKUP_FILE $COMPRESSED_FILE
