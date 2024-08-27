0607dac2b8a43cafd320d7eb2f190e2258b58fac
Marcelo Matos <marcelofmatos@gmail.com>
2024-08-27 11:15:38 +0000

    fix: directory name

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ ENV S3_BUCKET_NAME=your_s3_bucket_name
 ENV S3_REGION=your_s3_region
 ENV AWS_ACCESS_KEY_ID=your_access_key_id
 ENV AWS_SECRET_ACCESS_KEY=your_secret_access_key
+ENV S3_DIRECTORY_NAME=default-directory
 
 RUN mkdir -p /backup
 
--- backup.sh
+++ backup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Nome do arquivo de backup
 BACKUP_FILE="/backup/$(date +\%Y-\%m-\%d_\%H-\%M-\%S).sql"
 
-# Dump do banco de dados
+S3_DIRECTORY_NAME=${S3_DIRECTORY_NAME:-"default-directory"}
+
 pg_dump -Fc -f $BACKUP_FILE
 
-# Enviar o backup para o S3
-aws s3 cp $BACKUP_FILE s3://$S3_BUCKET_NAME/$(basename $BACKUP_FILE) --region $S3_REGION
+aws s3 cp $BACKUP_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$(basename $BACKUP_FILE) --region $S3_REGION
 
-# Remover o arquivo de backup local após o upload
 rm $BACKUP_FILE
