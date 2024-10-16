a09f25e704a3c04ddf677aa0dea1d4de4ad4f3a2
Marcelo Matos <marcelofmatos@gmail.com>
2024-10-16 12:28:01 +0000

    fix: default dump format

--- usr/local/bin/backup.sh
+++ usr/local/bin/backup.sh
@@ -5,7 +5,7 @@ COMPRESSED_FILE="/backup/$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tgz"
 
 S3_DIRECTORY_NAME=${S3_DIRECTORY_NAME:-"default-directory"}
 
-pg_dump -Fc -f $BACKUP_FILE
+pg_dump -f $BACKUP_FILE
 tar -czf $COMPRESSED_FILE -C /backup $(basename $BACKUP_FILE)
 
 aws s3 cp $COMPRESSED_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$(basename $COMPRESSED_FILE) --region $S3_REGION
