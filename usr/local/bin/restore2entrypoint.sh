699f51f18b965aee495768e05a170b86c8b0b567
Marcelo Matos <marcelofmatos@gmail.com>
2024-10-16 13:19:04 +0000

    fix: restore2entrypoint

--- usr/local/bin/restore2entrypoint.sh
+++ usr/local/bin/restore2entrypoint.sh
@@ -1,6 +1,3 @@
 FILENAME=$1
 aws s3 cp s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$FILENAME /docker-entrypoint-initdb.d/
-cd /docker-entrypoint-initdb.d/
-rm /docker-entrypoint-initdb.d/$FILENAME
-tar xzvf /docker-entrypoint-initdb.d/$FILENAME
 chmod +x /docker-entrypoint-initdb.d/*
