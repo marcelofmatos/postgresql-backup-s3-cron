c34fcf09fc2c3c9d2a1611b616c683da17ad0ee0
Marcelo Matos <marcelofmatos@gmail.com>
2024-10-16 13:24:54 +0000

    Update file backup.sh

--- usr/local/bin/backup.sh
+++ usr/local/bin/backup.sh
@@ -6,6 +6,6 @@ S3_DIRECTORY_NAME=${S3_DIRECTORY_NAME:-"default-directory"}
 
 pg_dumpall | gzip > $BACKUP_FILE
 
-aws s3 cp $BACKUP_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$BACKUP_FILE --region $S3_REGION
+aws s3 cp $BACKUP_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$(basename $BACKUP_FILE) --region $S3_REGION
 
 rm $BACKUP_FILE
