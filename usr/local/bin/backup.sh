9e714f08f8d4ea860b1b9de94e58cc124a952114
Marcelo Matos <marcelofmatos@gmail.com>
2024-10-16 13:40:44 +0000

    fix: PGDATABASE

--- usr/local/bin/backup.sh
+++ usr/local/bin/backup.sh
@@ -4,7 +4,7 @@ BACKUP_FILE="/backup/$(date +\%Y-\%m-\%d_\%H-\%M-\%S).sql.gz"
 
 S3_DIRECTORY_NAME=${S3_DIRECTORY_NAME:-"default-directory"}
 
-pg_dumpall | gzip > $BACKUP_FILE
+pg_dump $PGDATABASE | gzip > $BACKUP_FILE
 
 aws s3 cp $BACKUP_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$(basename $BACKUP_FILE) --region $S3_REGION
 
