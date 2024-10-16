ae0046e096fba62dd132d1089233a7b60df3dc15
Marcelo Matos <marcelofmatos@gmail.com>
2024-10-16 13:15:30 +0000

    fix: dump gzip

--- usr/local/bin/backup.sh
+++ usr/local/bin/backup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-BACKUP_FILE="/backup/$(date +\%Y-\%m-\%d_\%H-\%M-\%S).sql"
-COMPRESSED_FILE="/backup/$(date +\%Y-\%m-\%d_\%H-\%M-\%S).tgz"
+BACKUP_FILE="/backup/$(date +\%Y-\%m-\%d_\%H-\%M-\%S).sql.gz"
 
 S3_DIRECTORY_NAME=${S3_DIRECTORY_NAME:-"default-directory"}
 
-pg_dump -f $BACKUP_FILE
-tar -czf $COMPRESSED_FILE -C /backup $(basename $BACKUP_FILE)
+pg_dumpall | gzip > $BACKUP_FILE
 
-aws s3 cp $COMPRESSED_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$(basename $COMPRESSED_FILE) --region $S3_REGION
+aws s3 cp $BACKUP_FILE s3://$S3_BUCKET_NAME/$S3_DIRECTORY_NAME/$BACKUP_FILE --region $S3_REGION
 
-rm $BACKUP_FILE $COMPRESSED_FILE
+rm $BACKUP_FILE
